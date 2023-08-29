41b2acd6eca37478f1bf9c376fa6b0b1f794aa23
tanuushree <21cs240tanu@ug.cusat.ac.in>
2023-08-29 11:05:46 +0530

    tasks listed

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract todolist{
+contract TodoList{
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Add your first Task.");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
 }
